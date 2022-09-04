e21e3cbbc8ac81cf3e8cc2ce7abafaa5f07fd69a
JiSoo Hyeon <jichuuu0417@gmail.com>
2022-09-04 21:45:54 +0900

    feat:search

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,6 @@
+from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('', views.post_list,) # 함수 그 자체를 넘기는 것으로 호출 안되게 해야함
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+# 호출 당시에 현재 요청에 모든 내역을 인자로 전달 받음
+def post_list(request):
+    qs = Post.objects.all()
+    q = request.GET.get('q', '')
+    if q:
+        qs = qs.filter(message__icontains=q)
+    # render의 첫번째 인자는 view 함수의 request인자를 그대로 넘겨줌
+    # 두번째 인자는 템플릿명
+    # 세번째 인자로 dictionary로 qs라는 값을 참조할텐데 참조할 이름 지정
+    return render(request, 'instagram/post_list.html', {
+        'post_list' : qs,
+        'q' : q
+    })
