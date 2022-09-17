59a3f13c3462c5dd2565ed88f7eb9e6481171c3e
JiSoo Hyeon <jichuuu0417@gmail.com>
2022-09-17 23:43:03 +0900

    admin 수정

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .models import Post, Comment
+from .models import Post, Comment, Tag
 
 @admin.register(Post) # Wrapping 감싼 대상의 기능을 변강할 수 있음
 class PostAdmin(admin.ModelAdmin):
@@ -21,4 +21,8 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
